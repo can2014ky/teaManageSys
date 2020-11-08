fc8ebfa7eeda9a9e0c664ca7f2086072c794ffb3
can2014ky <can2014ky@163.com>
2020-11-08 10:17:26 +0800

    feat: 项目结构进行优化

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import SvgIcon from '@src/components/SvgIcon/Index.vue';
+import SvgIcon from './SvgIcon.vue';
 
 Vue.component('svg-icon', SvgIcon);
 
-const req = require.context('./svg', false, /\.svg$/);
+const req = require.context('./svg', true, /\.svg$/);
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 requireAll(req);
